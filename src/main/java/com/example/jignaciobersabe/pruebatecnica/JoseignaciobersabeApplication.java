1432a18ff432edf60088934173d6ac19afd759f9
Igna695 <igna695@gmail.com>
2023-10-09 22:54:51 +0200

    feat: ISSUExxx created skeleton to the project

---
+++ src/main/java/com/example/jignaciobersabe/pruebatecnica/JoseignaciobersabeApplication.java
@@ -0,0 +1,13 @@
+package com.example.jignaciobersabe.pruebatecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoseignaciobersabeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoseignaciobersabeApplication.class, args);
+	}
+
+}
